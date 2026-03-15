557141bf4f3996c03715c85c54f6e80996eb30fe
WrTP Webmaster <info@wearethepeople.us>
2026-03-15 12:14:28 -0500

    [2026-03-15] Basic publish/unpublish UI

--- app/routes/juxtapose.tsx
+++ app/routes/juxtapose.tsx
@@ -1,10 +1,27 @@
 import { Route } from './+types/juxtapose'
 import { getOrCreateParticipantSession } from '@/utils/participant-session.server.ts'
-import { getParticipantAllocation } from '@/utils/participants-db.server.ts'
-import { href, redirect } from 'react-router'
+import {
+	getParticipantAllocation,
+	getParticipantBySessionId,
+} from '@/utils/participants-db.server.ts'
+import { href, redirect, Form, data } from 'react-router'
 import { FUNCTIONS } from '@/constants/budget-functions.ts'
 import { getOmbBudgetByCodeForYear } from '@/utils/budget-data.ts'
 import { formatPercent, formatSignedPercent } from '@/utils/numbers.ts'
+import { Button } from '@/ui/button.tsx'
+import { getFormProps, getInputProps, useForm } from '@conform-to/react'
+import { z } from 'zod'
+import { getZodConstraint, parseWithZod } from '@conform-to/zod'
+import { getSessionId } from '@/utils/session.server.ts'
+
+const manageAllocationSchema = z.object({
+	intent: z.enum(['publish', 'unpublish']),
+	allocationId: z.string(),
+})
+
+const inputFormSchema = manageAllocationSchema.omit({
+	allocationId: true,
+})
 
 export async function loader({ request }: Route.LoaderArgs) {
 	const { headers, isNew, participantId } =
@@ -51,11 +68,86 @@ export async function loader({ request }: Route.LoaderArgs) {
 	)
 }
 
-export default function JuxtaposeRoute({ loaderData }: Route.ComponentProps) {
-	const { allocation, pairedData } = loaderData
+export async function action({ request }: Route.ActionArgs) {
+	const sessionId = await getSessionId(request)
+	const participant = await getParticipantBySessionId(sessionId)
 
-	console.log('allocation', allocation)
-	console.log('pairedData', pairedData)
+	if (!participant) {
+		return data({
+			resultType: 'error' as const,
+			result: {
+				status: 'error' as const,
+				error: {
+					'': ['Unable to identify participant.'],
+				},
+			},
+		})
+	}
+
+	const allocation = await getParticipantAllocation(participant.id)
+
+	if (!allocation) {
+		return data({
+			resultType: 'error' as const,
+			result: {
+				status: 'error' as const,
+				error: {
+					'': ['Unable to fetch allocation.'],
+				},
+			},
+		})
+	}
+
+	const formData = await request.formData()
+	formData.set('allocationId', allocation.id)
+
+	const submission = parseWithZod(formData, {
+		schema: manageAllocationSchema,
+	})
+
+	if (submission.status !== 'success') {
+		return data({
+			resultType: 'error' as const,
+			result: submission.reply({
+				formErrors: ['Unable to process allocation.'],
+			}),
+		})
+	}
+
+	return data({
+		resultType: 'success' as const,
+		result: submission.reply(),
+	})
+}
+
+export default function JuxtaposeRoute({
+	actionData,
+	loaderData,
+}: Route.ComponentProps) {
+	const { allocation, pairedData } = loaderData
+	const lastResult = actionData?.result
+	const publishState =
+		allocation.publicId && allocation.publishedAt && !allocation.unpublishedAt
+			? 'Published'
+			: 'Unpublished'
+	const publishButtonText =
+		publishState === 'Published' ? 'Unpublish' : 'Publish'
+
+	const [form, fields] = useForm({
+		defaultValue: {
+			intent: publishButtonText.toLowerCase(),
+		},
+		lastResult,
+		onValidate({ formData }) {
+			console.log(
+				'Validation',
+				parseWithZod(formData, { schema: inputFormSchema }),
+			)
+
+			return parseWithZod(formData, { schema: inputFormSchema })
+		},
+		constraint: getZodConstraint(inputFormSchema),
+	})
 
 	return (
 		<div>
@@ -82,6 +174,30 @@ export default function JuxtaposeRoute({ loaderData }: Route.ComponentProps) {
 					))}
 				</tbody>
 			</table>
+			<section className="mt-12">
+				<h2>Publish settings</h2>
+				<div className="flex flex-row gap-4">
+					<div className="flex grow flex-col gap-2">
+						<div>
+							Your budget allocation is currently:{' '}
+							<strong>{publishState}</strong>
+						</div>
+						<div id={form.errorId} className="text-red-500">
+							{form.errors}
+						</div>
+					</div>
+					<div>
+						<Form method="post" {...getFormProps(form)}>
+							<input
+								{...getInputProps(fields.intent, {
+									type: 'hidden',
+								})}
+							/>
+							<Button>{publishButtonText}</Button>
+						</Form>
+					</div>
+				</div>
+			</section>
 		</div>
 	)
 }
