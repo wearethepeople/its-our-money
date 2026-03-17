1581bb5e0fd84073e58a845b01812ffb035e7de9
WrTP Webmaster <info@wearethepeople.us>
2026-03-16 21:15:46 -0500

    [2026-03-16] One more ES thing to throw overboard for now

--- app/routes.ts
+++ app/routes.ts
@@ -7,7 +7,6 @@ export default [
 	route('s/:publicId', './routes/share-allocation.route.tsx'),
 	route('recover/:token', './routes/recovery.route.tsx'),
 	route('resources/healthcheck', './routes/resources/healthcheck.tsx'),
-	route('resources/images', './routes/resources/images.tsx'),
 	route('resources/theme-switch', './routes/resources/theme-switch.tsx'),
 	route('robots.txt', './routes/_seo/robots[.]txt.ts'),
 	route('sitemap.xml', './routes/_seo/sitemap[.]xml.ts'),
