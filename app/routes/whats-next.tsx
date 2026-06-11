b52e40baa23ee8930ecfa30e26940b72cffaca5b
WrTP Webmaster <info@wearethepeople.us>
2026-06-10 22:45:11 -0500

    [2026-06-10] WIP: What's next?

---
+++ app/routes/whats-next.tsx
@@ -0,0 +1,10 @@
+import { TypographyH1, TypographyLead } from "#app/components/ui/typography.tsx";
+
+export default function WhatsNextRoute() {
+  return (
+    <div>
+      <TypographyH1 className="mb-3">What's next?</TypographyH1>
+      <TypographyLead>What do you do now?</TypographyLead>
+    </div>
+  );
+}
